fd6804507a49cafa6aa828afcc0c061fa2288394
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-03 02:17:47 +0300

    fix: remove module-level throw in auth to fix vercel build

--- src/auth.ts
+++ src/auth.ts
@@ -3,23 +3,12 @@ import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/db";
 
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-
-
-
-if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-  console.log(GITHUB_CLIENT_ID);
-  console.log(GITHUB_CLIENT_SECRET);
-  throw new Error('Missing Github OAuth Credentials!');
-}
-
 export const { handlers: { GET, POST }, auth, signOut, signIn } = NextAuth({
   adapter: PrismaAdapter(db),
   providers: [
     GitHub({
-      clientId: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
     })
   ],
   callbacks: {
