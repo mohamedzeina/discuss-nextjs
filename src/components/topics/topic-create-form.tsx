5dd11ebe305e13da0772f87a39a1ca1e7a892e7f
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-06 17:31:23 +0300

    feat: standardise error message styling across all forms

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -43,11 +43,14 @@ export default function TopicCreateForm() {
               errorMessage={formState.errors.description?.join(', ')}
             />
 
-            {formState.errors._form ? (
-              <div className="p-2 bg-red-200 border rounded border-red-400">
-                {formState.errors._form?.join(', ')}
+            {formState.errors._form && (
+              <div className="flex items-start gap-2 p-3 bg-red-50 border border-red-300 rounded-lg text-red-700 text-sm">
+                <svg className="w-4 h-4 mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm-.75-4.75a.75.75 0 001.5 0v-4.5a.75.75 0 00-1.5 0v4.5zm.75-7.5a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+                </svg>
+                {formState.errors._form.join(', ')}
               </div>
-            ) : null}
+            )}
 
             <FormButton>Save</FormButton>
           </div>
